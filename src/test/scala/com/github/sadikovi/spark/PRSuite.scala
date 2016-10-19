f55e322eec20389da994faf47eba0fe22ca06d68
Ivan Sadikov <isadikov@wynyardgroup.com>
2016-10-20 08:38:08 +1300

    add dummy test

--- src/test/scala/com/github/sadikovi/spark/PRSuite.scala
+++ src/test/scala/com/github/sadikovi/spark/PRSuite.scala
@@ -14,20 +14,12 @@
  * limitations under the License.
  */
 
-package com.github.sadikovi.testutil
+package com.github.sadikovi.spark
 
-import java.io.File
+import com.github.sadikovi.testutil.{UnitTestSuite, SparkLocal}
 
-import org.scalatest._
-
-package object implicits {
-  implicit class PathBuilder(path: String) {
-    def /(suffix: String): String = path + File.separator + suffix
-
-    def `:`(append: String): String = path + File.pathSeparator + append
+class PRSuite extends UnitTestSuite with SparkLocal {
+  test("simple test") {
+    true should be (true)
   }
 }
-
-/** abstract general testing class */
-abstract class UnitTestSuite extends FunSuite with Matchers with OptionValues with Inside
-  with Inspectors with TestBase with BeforeAndAfterAll with BeforeAndAfter
